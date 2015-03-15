08ca21317c71bde39ea5110a432919ed25e0ac47
Maxwell Pray <synthead@gmail.com>
2015-03-14 19:39:52 -0700

    Remove MCP3008 CS low sleep and sample averaging.

--- mcp3008.cpp
+++ mcp3008.cpp
@@ -1,27 +1,19 @@
 #include "mcp3008.h"
 #include <SPI.h>
 
-#define MCP3008_CS_WAIT 4
-#define MCP3008_SAMPLES 5
-
 void mcp3008_setup(int chip_select) {
   pinMode(chip_select, OUTPUT);
   digitalWrite(chip_select, HIGH);
 };
 
 int mcp3008_read(int chip_select, int channel, int range) {
-  long sample_sum = 0;
+  digitalWrite(chip_select, LOW);
 
-  for (int sample = 0; sample < MCP3008_SAMPLES; sample++) {
-    digitalWrite(chip_select, LOW);
-    delayMicroseconds(MCP3008_CS_WAIT);
+  SPI.transfer((channel << 2) + 0b1100000);
+  long value = SPI.transfer(0) << 2;
+  value += (SPI.transfer(0) & 0b11000000) >> 6;
 
-    SPI.transfer((channel << 2) + 0b1100000);
-    sample_sum += SPI.transfer(0) << 2;
-    sample_sum += (SPI.transfer(0) & 0b11000000) >> 6;
+  digitalWrite(chip_select, HIGH);
 
-    digitalWrite(chip_select, HIGH);
-  }
-
-  return (sample_sum / MCP3008_SAMPLES) * range / 1023;
+  return value * range / 1023;
 }
