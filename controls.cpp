e06e5f95a13cde896be044261e91b3a5fbba1d7a
Maxwell Pray <synthead@gmail.com>
2015-03-14 22:03:42 -0700

    Screen writes title once when parameter is being tuned.

--- controls.cpp
+++ controls.cpp
@@ -11,9 +11,14 @@
 #define ANALOG_CONTROL_COUNT 14
 #define DIGITAL_CONTROL_COUNT 8
 
+struct analog_change {
+  uint8_t chip_select;
+  uint8_t pin;
+};
+
 struct analog_control {
   const uint8_t chip_select;
-  const uint8_t analog_pin;
+  const uint8_t pin;
   const uint8_t range;
   void (*callback)(uint8_t);
   const char* line1;
@@ -22,16 +27,23 @@ struct analog_control {
 };
 
 struct digital_control {
-  const uint8_t bit;
+  const uint8_t pin;
   void (*callback)(bool);
   const char* line1;
   const char* line2;
   bool value;
 };
 
+struct analog_change last_analog_change = {
+    .chip_select = -1,
+    .pin = -1
+};
+
+uint8_t last_digital_change_pin = -1;
+
 struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
     {.chip_select = MCP3008_0_CS,
-     .analog_pin = 0,
+     .pin = 0,
      .range = RANGE_MULTI_SAMPLE_WAVE,
      .callback = multi_sample_wave_carrier,
      .line1 = "Multi-smple wave",
@@ -39,7 +51,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_0_CS,
-     .analog_pin = 1,
+     .pin = 1,
      .range = RANGE_MULTI_SAMPLE_WAVE,
      .callback = multi_sample_wave_modulation,
      .line1 = "Multi-smple wave",
@@ -47,7 +59,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_0_CS,
-     .analog_pin = 2,
+     .pin = 2,
      .range = RANGE_MODULATION_INDEX,
      .callback = modulation_index,
      .line1 = "Modulation index",
@@ -55,7 +67,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_0_CS,
-     .analog_pin = 3,
+     .pin = 3,
      .range = RANGE_FM_FEEDBACK_CONSTANT,
      .callback = fm_feedback_constant,
      .line1 = "FM feedback",
@@ -63,7 +75,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_0_CS,
-     .analog_pin = 4,
+     .pin = 4,
      .range = RANGE_LEVEL_KEY_SCALE,
      .callback = level_key_scale_carrier,
      .line1 = "LEVEL key scale",
@@ -71,7 +83,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_0_CS,
-     .analog_pin = 5,
+     .pin = 5,
      .range = RANGE_LEVEL_KEY_SCALE,
      .callback = level_key_scale_modulation,
      .line1 = "LEVEL key scale",
@@ -79,7 +91,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_1_CS,
-     .analog_pin = 0,
+     .pin = 0,
      .range = RANGE_ADSR_RATE,
      .callback = attack_rate_carrier,
      .line1 = "Attack",
@@ -87,7 +99,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_1_CS,
-     .analog_pin = 1,
+     .pin = 1,
      .range = RANGE_ADSR_RATE,
      .callback = decay_rate_carrier,
      .line1 = "Decay",
@@ -95,7 +107,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_1_CS,
-     .analog_pin = 2,
+     .pin = 2,
      .range = RANGE_ADSR_RATE,
      .callback = sustain_rate_carrier,
      .line1 = "Sustain",
@@ -103,7 +115,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_1_CS,
-     .analog_pin = 3,
+     .pin = 3,
      .range = RANGE_ADSR_RATE,
      .callback = release_rate_carrier,
      .line1 = "Release",
@@ -111,7 +123,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_1_CS,
-     .analog_pin = 4,
+     .pin = 4,
      .range = RANGE_ADSR_RATE,
      .callback = attack_rate_modulation,
      .line1 = "Attack",
@@ -119,7 +131,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_1_CS,
-     .analog_pin = 5,
+     .pin = 5,
      .range = RANGE_ADSR_RATE,
      .callback = decay_rate_modulation,
      .line1 = "Decay",
@@ -127,7 +139,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_1_CS,
-     .analog_pin = 6,
+     .pin = 6,
      .range = RANGE_ADSR_RATE,
      .callback = sustain_rate_modulation,
      .line1 = "Sustain",
@@ -135,7 +147,7 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
      .value = 0},
 
     {.chip_select = MCP3008_1_CS,
-     .analog_pin = 7,
+     .pin = 7,
      .range = RANGE_ADSR_RATE,
      .callback = release_rate_modulation,
      .line1 = "Release",
@@ -144,89 +156,103 @@ struct analog_control analog_controls[ANALOG_CONTROL_COUNT] = {
 };
 
 struct digital_control digital_controls[DIGITAL_CONTROL_COUNT] = {
-    {.bit = 0,
+    {.pin = 0,
      .callback = amplitude_modulation_carrier,
      .line1 = "Amplitude Modltn",
      .line2 = "Carrier",
      .value = false},
 
-    {.bit = 1,
+    {.pin = 1,
      .callback = vibrato_carrier,
      .line1 = "Vibrato",
      .line2 = "Carrier",
      .value = false},
 
-    {.bit = 2,
+    {.pin = 2,
      .callback = wave_distortion_carrier,
      .line1 = "Wave distortion",
      .line2 = "Carrier",
      .value = false},
 
-    {.bit = 3,
+    {.pin = 3,
      .callback = rate_key_scale_carrier,
      .line1 = "RATE key scale",
      .line2 = "Carrier",
      .value = false},
 
-    {.bit = 4,
+    {.pin = 4,
      .callback = amplitude_modulation_modulation,
      .line1 = "Amplitude Modltn",
      .line2 = "Modulation",
      .value = false},
 
-    {.bit = 5,
+    {.pin = 5,
      .callback = vibrato_modulation,
      .line1 = "Vibrato",
      .line2 = "Modulation",
      .value = false},
 
-    {.bit = 6,
+    {.pin = 6,
      .callback = wave_distortion_modulation,
      .line1 = "Wave distortion",
      .line2 = "Modulation",
      .value = false},
 
-    {.bit = 7,
+    {.pin = 7,
      .callback = rate_key_scale_modulation,
      .line1 = "RATE key scale",
      .line2 = "Modulation",
      .value = false}
 };
 
-void lcd_print_analog_control(
-    const char* line1, const char* line2, unsigned int value,
-    unsigned int range) {
-  char line1_padded[17];
-  sprintf(line1_padded, "%-16s", line1);
-  lcd_print_position(0, 0, line1_padded);
-
-  char line2_padded[13];
-  sprintf(line2_padded, "%-12s", line2);
-  lcd_print_position(0, 1, line2_padded);
-
+void lcd_print_analog_control(analog_control control) {
   char percent_text[5];
-  sprintf(percent_text, "%3d%%", value * 100 / range);
-  lcd_print(percent_text);
+  sprintf(percent_text, "%3d%%", control.value * 100 / control.range);
+
+  if (last_analog_change.chip_select != control.chip_select ||
+      last_analog_change.pin != control.pin) {
+    last_analog_change.chip_select = control.chip_select;
+    last_analog_change.pin = control.pin;
+
+    char line1_padded[17];
+    sprintf(line1_padded, "%-16s", control.line1);
+    lcd_print_position(0, 0, line1_padded);
+
+    char line2_padded[13];
+    sprintf(line2_padded, "%-12s", control.line2);
+    lcd_print_position(0, 1, line2_padded);
+
+    lcd_print(percent_text);
+  } else {
+    lcd_print_position(12, 1, percent_text);
+  }
 }
 
-void lcd_print_digital_control(
-    const char* line1, const char* line2, bool value) {
-  char line1_padded[17];
-  sprintf(line1_padded, "%-16s", line1);
-  lcd_print_position(0, 0, line1_padded);
+void lcd_print_digital_control(digital_control control) {
+  const char* state = control.value ? "On " : "Off";
 
-  char line2_padded[14];
-  sprintf(line2_padded, "%-13s", line2);
-  lcd_print_position(0, 1, line2_padded);
+  if (last_digital_change_pin != control.pin) {
+    last_digital_change_pin = control.pin;
 
-  lcd_print(value ? "On " : "Off");
+    char line1_padded[17];
+    sprintf(line1_padded, "%-16s", control.line1);
+    lcd_print_position(0, 0, line1_padded);
+
+    char line2_padded[14];
+    sprintf(line2_padded, "%-13s", control.line2);
+    lcd_print_position(0, 1, line2_padded);
+
+    lcd_print(state);
+  } else {
+    lcd_print_position(13, 1, state);
+  }
 }
 
 void apply_analog_controls(bool print_to_lcd) {
   for (unsigned int control = 0; control < ANALOG_CONTROL_COUNT; control++) {
     unsigned int value = mcp3008_read(
         analog_controls[control].chip_select,
-        analog_controls[control].analog_pin,
+        analog_controls[control].pin,
         analog_controls[control].range);
 
     if (value != analog_controls[control].value) {
@@ -234,11 +260,7 @@ void apply_analog_controls(bool print_to_lcd) {
       analog_controls[control].callback(value);
 
       if (print_to_lcd) {
-        lcd_print_analog_control(
-            analog_controls[control].line1,
-            analog_controls[control].line2,
-            value,
-            analog_controls[control].range);
+        lcd_print_analog_control(analog_controls[control]);
       }
     }
   }
@@ -248,17 +270,14 @@ void apply_digital_controls(bool print_to_lcd) {
   uint8_t values = mcp23s17_read(MCP23S17_HW_ADDRESS, 0x13);
 
   for (unsigned int control = 0; control < DIGITAL_CONTROL_COUNT; control++) {
-    bool value = (values >> digital_controls[control].bit) & 1;
+    bool value = (values >> digital_controls[control].pin) & 1;
 
     if (value != digital_controls[control].value) {
       digital_controls[control].value = value;
       digital_controls[control].callback(value);
 
       if (print_to_lcd) {
-        lcd_print_digital_control(
-            digital_controls[control].line1,
-            digital_controls[control].line2,
-            value);
+        lcd_print_digital_control(digital_controls[control]);
       }
     }
   }
