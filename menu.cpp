a129915742307bed75e789f22c209ad8b643c552
Maxwell Pray <synthead@gmail.com>
2015-03-21 00:40:27 -0700

    Add support for reading menu controls including a rotary encoder.

--- menu.cpp
+++ menu.cpp
@@ -1,11 +1,21 @@
 #include "menu.h"
 #include "hd44780.h"
+#include "mcp23s17.h"
 #include <Arduino.h>
+
+#define MENU_MATRIX_COLUMN 7
+
+#define INPUTS_ENCODER_CCW  0b00000001
+#define INPUTS_ENCODER_CW   0b00000010
+#define INPUTS_ENCODER_DOWN 0b00000100
 
 #define TEMP_MESSAGE_EXPIRATION 3000
 
 namespace Menu {
   temporary_message_t temporary_message;
+
+  uint8_t read_encoder = false;
+  uint8_t last_inputs = 0;
 
   void startup_message() {
     HD44780::print_position(0, 0, "Synthead YM2420 ");
@@ -27,5 +37,29 @@ namespace Menu {
       HD44780::print_position(0, 0, "000: I made it! ");
       HD44780::print_position(0, 1, "                ");
     }
+  }
+
+  uint8_t scan_inputs() {
+    uint8_t inputs = MCP23S17::scan_matrix(MENU_MATRIX_COLUMN);
+    uint8_t new_inputs = inputs & ~(inputs & last_inputs) & 0b11111100;
+
+    uint8_t encoder = inputs & 0b11;
+    if (read_encoder && encoder == 0b00) {
+      switch (last_inputs & 0b11) {
+        case 0b01:
+          new_inputs |= INPUTS_ENCODER_CCW;
+          break;
+        case 0b10:
+          new_inputs |= INPUTS_ENCODER_CW;
+          break;
+      }
+
+      read_encoder = false;
+    } else if (! read_encoder && encoder == 0b11) {
+      read_encoder = true;
+    }
+
+    last_inputs = inputs;
+    return new_inputs;
   }
 }
